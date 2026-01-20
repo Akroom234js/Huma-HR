42380eb1f7ec671fe62060e59815578e5932ab21
Akroom234js <ww232751@gamil.com>
2026-01-20 11:28:01 +0300

    fix small bug

--- FrontEnd/src/Components/Pages/Recrutment/ToScheduleInterview/ToScheduleInterview.jsx
+++ FrontEnd/src/Components/Pages/Recrutment/ToScheduleInterview/ToScheduleInterview.jsx
@@ -10,114 +10,114 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import ScheduleInterview from '../ScheduleInterview/ScheduleInterview';
 
-export default function ToScheduleInterview(){
-     const [activeTab, setActiveTab] = useState('schedule-interview');
-        const [selectedDepartment1, setSelectedDepartment1] = useState('');
-        
-        const {t}=useTranslation("Recrutment/ToScheduleInterview")
-        const tabs = [
-            { id: 'interview-happening', label: t('Tabs.Interview-Happening'), count: 3 },
-            { id: 'schedule-interview', label: t('Tabs.To-Schedule-Interview'), count: 8 },
-            { id: 'make-offer', label: t('Tabs.To-Make-Offer'), count: 6 },
-        { id: 'opening-jobs', label: t('Tabs.Opening'), count: 3, path: '/recruitment/opening-jobs' },
-        ];
-    
-        const departmentOptions = [
-            { value: '', label: t('departmentOptions.all')},
-            { value: 'engineering', label: 'Engineering' },
-            { value: 'design', label: 'Design' },
-            { value: 'product', label: 'Product Management' },
-            { value: 'marketing', label: 'Marketing' },
-        ];
-    
-        const candidates = [
-            {
-                id: 1,
-                name: 'James Smith1',
-                department: 'Engineering',
-                position: 'Senior Frontend Developer',
-                score: 92,
-                skills: ['React', 'TypeScript'],
-                att:['pdf','png']
-            },
-            {
-                id: 2,
-                name: 'Anna Lee1',
-                department: 'Design',
-                position: 'Product Designer',
-                score: 88,
-                skills: ['Figma', 'UI/UX'],
-                att:['pdf','png']
-            },
-            {
-                id: 3,
-                name: 'Robert Fox',
-                department: 'Product',
-                position: 'Product Manager',
-                score: 74,
-                skills: ['Agile', 'Scrum'],
-                att:['pdf','png']
-            },
-            {
-                id: 4,
-                name: 'Maria Klein',
-                department: 'Marketing',
-                position: 'Marketing Specialist',
-                score: 81,
-                skills: ['SEO', 'Content'],
-                att:['word','png']
-            },
-            {
-                id: 5,
-                name: 'David Thompson',
-                department: 'Engineering',
-                position: 'DevOps Engineer',
-                score: 45,
-                skills: ['AWS', 'Docker'],
-                att:['pdf','zip']
-            },
-            {
-                id: 6,
-                name: 'Lisa Chen',
-                department: 'Design',
-                position: 'UX Researcher',
-                score: 95,
-                skills: ['Research', 'Testing'],
-                att:['pdf','png']
-            }
-        ];
-    return(<>
- 
-     <div className="recruitment-page ">
-        
-                <div className="recruitment-container">
-                    <div className="recruitment-header-flex">
-                        <Header />
-                        <ThemeToggle />
-                    </div>
-                    <Tabs tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
-    
-                    <FilterDropdown
-                        value={selectedDepartment1}
-                        onChange={setSelectedDepartment1}
-                        options={departmentOptions}
-                    />
-    
-                    <div className="candidates-grid">
-                        {candidates.map((candidate) => (
-                            <CandidateCardToScheduleInterview key={candidate.id} candidate={candidate}/>
-                            // <CandidateCard key={candidate.id} candidate={candidate}/>
-                        ))}
-                    </div>
-    
-                    <div className="view-more">
-                        <button className="view-more-btn">
-                           { t('applicants')}
-                            <span className="material-symbols-outlined">expand_more</span>
-                        </button>
-                    </div>
+export default function ToScheduleInterview() {
+    const [activeTab, setActiveTab] = useState('schedule-interview');
+    const [selectedDepartment1, setSelectedDepartment1] = useState('');
 
-                
+    const { t } = useTranslation("Recrutment/ToScheduleInterview")
+    const tabs = [
+        { id: 'interview-happening', label: t('Tabs.Interview-Happening'), count: 3, path: '/recruitment/interview-happening' },
+        { id: 'schedule-interview', label: t('Tabs.To-Schedule-Interview'), count: 8, path: '/recruitment/schedule-interview' },
+        { id: 'make-offer', label: t('Tabs.To-Make-Offer'), count: 6, path: '/recruitment/make-offer' },
+        { id: 'opening-jobs', label: t('Tabs.Opening'), count: 3, path: '/recruitment/opening-jobs' },
+    ];
+
+    const departmentOptions = [
+        { value: '', label: t('departmentOptions.all') },
+        { value: 'engineering', label: 'Engineering' },
+        { value: 'design', label: 'Design' },
+        { value: 'product', label: 'Product Management' },
+        { value: 'marketing', label: 'Marketing' },
+    ];
+
+    const candidates = [
+        {
+            id: 1,
+            name: 'James Smith1',
+            department: 'Engineering',
+            position: 'Senior Frontend Developer',
+            score: 92,
+            skills: ['React', 'TypeScript'],
+            att: ['pdf', 'png']
+        },
+        {
+            id: 2,
+            name: 'Anna Lee1',
+            department: 'Design',
+            position: 'Product Designer',
+            score: 88,
+            skills: ['Figma', 'UI/UX'],
+            att: ['pdf', 'png']
+        },
+        {
+            id: 3,
+            name: 'Robert Fox',
+            department: 'Product',
+            position: 'Product Manager',
+            score: 74,
+            skills: ['Agile', 'Scrum'],
+            att: ['pdf', 'png']
+        },
+        {
+            id: 4,
+            name: 'Maria Klein',
+            department: 'Marketing',
+            position: 'Marketing Specialist',
+            score: 81,
+            skills: ['SEO', 'Content'],
+            att: ['word', 'png']
+        },
+        {
+            id: 5,
+            name: 'David Thompson',
+            department: 'Engineering',
+            position: 'DevOps Engineer',
+            score: 45,
+            skills: ['AWS', 'Docker'],
+            att: ['pdf', 'zip']
+        },
+        {
+            id: 6,
+            name: 'Lisa Chen',
+            department: 'Design',
+            position: 'UX Researcher',
+            score: 95,
+            skills: ['Research', 'Testing'],
+            att: ['pdf', 'png']
+        }
+    ];
+    return (<>
+
+        <div className="recruitment-page ">
+
+            <div className="recruitment-container">
+                <div className="recruitment-header-flex">
+                    <Header />
+                    <ThemeToggle />
+                </div>
+                <Tabs tabs={tabs} activeTab={activeTab} onTabChange={setActiveTab} />
+
+                <FilterDropdown
+                    value={selectedDepartment1}
+                    onChange={setSelectedDepartment1}
+                    options={departmentOptions}
+                />
+
+                <div className="candidates-grid">
+                    {candidates.map((candidate) => (
+                        <CandidateCardToScheduleInterview key={candidate.id} candidate={candidate} />
+                        // <CandidateCard key={candidate.id} candidate={candidate}/>
+                    ))}
+                </div>
+
+                <div className="view-more">
+                    <button className="view-more-btn">
+                        {t('applicants')}
+                        <span className="material-symbols-outlined">expand_more</span>
+                    </button>
+                </div>
+
+
             </div>
         </div>
     </>)
