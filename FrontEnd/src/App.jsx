3b973cdd42d74b3ea80a5c709edd341ce40f1737
Akroom234js <ww232751@gamil.com>
2026-01-20 13:40:13 +0300

    Set Home component routes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,29 +2,39 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './Components/Sidebar/sidebar';
 import Recruitment from './Components/Pages/Recrutment/Main-page/Recrutment';
-import ScheduleInterview from './Components/Pages/Recrutment/ScheduleInterview/ScheduleInterview';
-import LanSw from './Components/LanSw'
+import Home from './Components/Pages/Home/Home';
+import Jops from './Components/Pages/Home/Jops';
 import './App.css';
 import ToScheduleInterview from './Components/Pages/Recrutment/ToScheduleInterview/ToScheduleInterview';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app-container">
-        <Sidebar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Recruitment />} />
-            <Route path="/recruitment" element={<Recruitment />} />
-            <Route path="/recruitment/make-offer" element={<Recruitment />} />
-            <Route path="/recruitment/opening-jobs" element={<Recruitment />} />
-            <Route path="/recruitment/schedule-interview" element={<ToScheduleInterview />} />
-            <Route path="/recruitment/interview-happening" element={<Recruitment />} /> {/* Or other component if exists */}
-          </Routes>
-        </main>
-        {/* <ScheduleInterview/> */}
-        {/* <LanSw/> */}
-      </div>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/jops" element={<Jops />} />
+
+        {/* Dashboard Routes with Sidebar */}
+        <Route
+          path="/recruitment/*"
+          element={
+            <div className="app-container">
+              <Sidebar />
+              <main className="main-content">
+                <Routes>
+                  <Route path="/" element={<Recruitment />} />
+                  <Route path="make-offer" element={<Recruitment />} />
+                  <Route path="opening-jobs" element={<Recruitment />} />
+                  <Route path="schedule-interview" element={<ToScheduleInterview />} />
+                  <Route path="interview-happening" element={<Recruitment />} />
+                </Routes>
+              </main>
+            </div>
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
