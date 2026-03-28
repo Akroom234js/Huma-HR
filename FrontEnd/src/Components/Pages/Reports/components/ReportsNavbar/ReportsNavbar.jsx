ae1a4baa69b831de3f2a082cf5edb5cb65d8854a
hadeel alhasan <hadeelalhasan111@gmail.com>
2026-03-28 11:30:19 +0300

    Translation of the ReportsNavbar page.

--- FrontEnd/src/Components/Pages/Reports/components/ReportsNavbar/ReportsNavbar.jsx
+++ FrontEnd/src/Components/Pages/Reports/components/ReportsNavbar/ReportsNavbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './ReportsNavbar.css';
-
+import { useTranslation } from 'react-i18next';
 const ReportsNavbar = () => {
+    const {t}=useTranslation("Reports/ReportsNavbar")
     return (
         <nav className="reports-navbar">
-            <NavLink to="/reports/payroll" className={({ isActive }) => isActive ? 'active' : ''}>Payroll Reports</NavLink>
-            <NavLink to="/reports/performance" className={({ isActive }) => isActive ? 'active' : ''}>Performance Reports</NavLink>
-            <NavLink to="/reports/leaves" className={({ isActive }) => isActive ? 'active' : ''}>Leaves Reports</NavLink>
-            <NavLink to="/reports/attendance" className={({ isActive }) => isActive ? 'active' : ''}>Attendance Tracking</NavLink>
-            <NavLink to="/reports/employees" className={({ isActive }) => isActive ? 'active' : ''}>Employee Reports</NavLink>
+            <NavLink to="/reports/payroll" className={({ isActive }) => isActive ? 'active' : ''}>{t("PayrollReports")}</NavLink>
+            <NavLink to="/reports/performance" className={({ isActive }) => isActive ? 'active' : ''}>{t("PerformanceReports")}</NavLink>
+            <NavLink to="/reports/leaves" className={({ isActive }) => isActive ? 'active' : ''}>{t("LeavesReports")}</NavLink>
+            <NavLink to="/reports/attendance" className={({ isActive }) => isActive ? 'active' : ''}>{t("AttendanceTracking")}</NavLink>
+            <NavLink to="/reports/employees" className={({ isActive }) => isActive ? 'active' : ''}>{t("EmployeeReports")}</NavLink>
         </nav>
     );
 };
