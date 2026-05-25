0914aae92f48bfa93fae55908dac2c791a2166be
Akroom234js <ww232751@gamil.com>
2026-05-25 12:29:40 +0300

    feat(frontend-config): define dynamic STORAGE_BASE_URL to support global servers

--- FrontEnd/src/apiConfig.js
+++ FrontEnd/src/apiConfig.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const API_BASE_URL = "http://127.0.0.1:8000/api";
 // export const API_BASE_URL = "https://huma-hr.onrender.com/api";
 
+export const STORAGE_BASE_URL = API_BASE_URL.replace('/api', '/storage');
+
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
