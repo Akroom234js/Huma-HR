08a63ff8d88ab6546bddd511937ce289472b26ff
Akroom234js <ww232751@gamil.com>
2026-05-28 20:28:33 +0300

    feat(frontend): connect hr requests list and cards with status update actions

--- FrontEnd/src/Components/Pages/Requests/Requests/RequestList/RequestList.jsx
+++ FrontEnd/src/Components/Pages/Requests/Requests/RequestList/RequestList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './RequestList.css';
 import RequestCard from '../RequestCard/RequestCard';
 
-const RequestList = ({ requests }) => {
+const RequestList = ({ requests, onAction }) => {
     return (
         <div className="req-list">
             {requests.map((request, index) => (
-                <RequestCard key={index} request={request} />
+                <RequestCard key={index} request={request} onAction={onAction} />
             ))}
         </div>
     );
