608dc5694b40401cc6f8dbfa769954069d45cc2f
hadeel alhasan <hadeelalhasan111@gmail.com>
2026-06-04 20:39:33 +0300

    start,edit,delete cycle

--- FrontEnd/src/Components/Pages/HRPerformance/PerformanceReports/PerformanceReports.jsx
+++ FrontEnd/src/Components/Pages/HRPerformance/PerformanceReports/PerformanceReports.jsx
@@ -3,20 +3,54 @@ import './PerformanceReports.css';
 import { useTranslation } from 'react-i18next';
 import ThemeToggle from '../../../ThemeToggle/ThemeToggle';
 import ReportsTable from './ReportsTable';
-
+import { Modal } from 'bootstrap';
+import CycleModal from './CycleModal';
+import Message from './Message';
+    // ── Cycle Modal Logic ─────────────────────────────────────────
+    const show=()=>{
+      console.log('fdfdf')
+                const container = document.querySelector('.cycle-modal-hr');
+            if (container) {
+                document.body.style.overflow = 'hidden';
+                container.style.display = 'flex';
+             
+            }
+    }
+    const message=()=>{
+      console.log('fdfdf')
+                const container = document.querySelector('.del-cycle-hr');
+            if (container) {
+                document.body.style.overflow = 'hidden';
+                container.style.display = 'flex';
+             
+            }
+    }
 const PerformanceReports = () => {
     const {t}=useTranslation("HrPerformance/PerformanceReports")
 
     return (
         <div className="performance-container PerformanceReports-container">
+          <div className="cycle-modal-hr">
+            <CycleModal/>
+          </div>
+          <div className="del-cycle-hr">
+            <Message/>
+          </div>
           <div className='fl-per'>
       <div>
           <h1>{t("title")}</h1>
           <p>{t("des")}</p>
       </div>
-            <button className="btn btn-secondary pos-per" onclick="exportReportsCSV()">
+        <div>
+              <button className="btn btn-secondary pos-per" onClick="exportReportsCSV()">
           <i className="fa-solid fa-file-export"></i> {t("Export")}
         </button>
+        <button className="btn btn-start-cycle" onClick={()=>show()} id="startCycleBtn">
+            <i className="fa-solid fa-circle-play"></i>
+            <span>Start New Cycle</span>
+            <span className="pulse-dot"></span>
+          </button>
+        </div>
           </div>
      
            <div className="em-theme-toggle-wrapper">
@@ -26,14 +60,18 @@ const PerformanceReports = () => {
 
 
            <div className="card">
-        <div className="card-title">
-          <span>{t("Consolidated")}</span>
+        <div className="card-title fl-hr-re">
+        <div>  <span>{t("Consolidated")}</span>
           <div className="filter-group">
             <span className="filter-label">{t("Selection")}</span>
             <select className="select-input" >
               <option selected>{t("First")} 2026 (Q1)</option>
               <option>{t("Fourth")} 2025 (Q4)</option>
             </select>
+          </div></div>
+          <div>
+            <button className='btn btn-start-cycle btn-edit-cycle' onClick={()=>show()}>{t("edit")}</button>
+            <button className='btn btn-start-cycle btn-del-cycle' onClick={()=>message()}>{t("delete")}</button>
           </div>
         </div>
 
